dd6eaf7e1ac7f67d5f89fd45332ae159fffc401e
lizhouyang <lizhy.gis@gmail.com>
2014-12-10 10:39:46 +0800

    首次提交，pom.xml首次确定

---
+++ src/main/java/com/company/dataHandler/App.java
@@ -0,0 +1,26 @@
+package com.company.dataHandler;
+
+import java.util.Arrays;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        System.out.println("Let's inspect the beans provided by Spring:");
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
---
+++ src/main/java/com/company/dataHandler/SocketEntrance.java
@@ -0,0 +1,8 @@
+package com.company.dataHandler;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SocketEntrance {
+
+}
